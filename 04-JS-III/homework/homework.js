27e947ee1cd658e5f2435dd02ef0903943c91368
Agustin <luisagustingonzalez26@gmail.com>
2022-09-20 23:43:50 -0300

    04-JS-III/homework/homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -225,7 +225,7 @@ function mesesDelAño(array) {
       resultado.push(array[i])
     }
   }
-  
+
   if (resultado.length != 3){
     return "No se encontraron los meses pedidos"
   }
@@ -285,6 +285,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for(var i= 0; i<10; i++) {
+    if(i === 5) continue;
+    else {
+      suma = suma + 2;
+      array.push(suma);
+    }
+  }
+  return array;
 }
 
 
